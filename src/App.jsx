48c41df039d10237c76e0c3e5c120c50bdb7585a
Abdulatif <abdulatifa801@gmail.com>
2025-06-17 15:38:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Market from './componenets/Market'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <Market />
+    </div>
+  )
+}
+
+export default App
